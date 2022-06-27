942da5c35e351edfd1630ca72f40633f71fc4772
StevenSermeus <67290591+StevenSermeus@users.noreply.github.com>
2022-06-27 17:07:01 +0200

    remove socket io + fix issue

--- projetIA/static/script.js
+++ projetIA/static/script.js
@@ -1,37 +1,48 @@
-var io = io();
+function handleMove(move) {
+    axios.post('http://127.0.0.1:5000/move', {
+        move: move
+    }).then(function (response) {
+        console.log(response)
+        if(response.data.status == 200){
+            document.getElementsByClassName('player2Current')[0].classList.add('player2');
+            document.getElementsByClassName('player2Current')[0].classList.remove('player2Current');
+            for(let enclos_case of response.data.captured_position){
+                document.getElementById(`c${enclos_case.x+1}r${enclos_case.y+1}`).classList.add('player2');
+            }
+            document.getElementById(`c${response.data.new_position.x+1}r${response.data.new_position.y+1}`).classList.add('player2Current');
+            getMove();
+            return;
 
-function setBoxesToPlayer(player,last_move, ...boxes){
-	let element = document.getElementById(last_move).classList
-	element.remove(player+"Current")
-	element.remove(player)
-	element.add(player)
-	for (const box of boxes) {
-		let element = document.getElementById(box).classList;
-		console.log(element);
-		element.remove('neutral');
-		element.add(player+"Current");
-	}
+        }
+        if(response.data.status == 201){
+            //redirect to result page
+            window.location.href = "http://127.0.0.1:5000/result";
+            return
+        }
+            alert("Move is wrong")
+
+    }).catch(function (error) {
+        console.log(error);
+    }).then(function () {
+        // always executed
+    });
 }
 
-
-
-const socket = io.connect(window.location.origin);
-
-
-socket.on('connect', function() {
-	//enable the arrow
-});
-socket.on('moveChecked',(past_move1,move_1,past_move2,move_2)=>{
-  //color case if api response is a move else message
-	setBoxesToPlayer('player1',`c${past_move1.y+1}r${past_move1.x+1}`,[`c${move_1.y+1}r${move_1.x+1}`])
-	setBoxesToPlayer('player2',`c${past_move2.y+1}r${past_move2.x+1}`,[`c${move_2.y+1}r${move_2.x+1}`])
-});
-
-function emitMove(move){
-  //disable arrow
-	console.log(move)
-	socket.emit('move',move);
-}
-function requestMoveAi(){
-//socket.emit('aiMove');
+function getMove(){
+    axios.get('http://127.0.0.1:5000/move').then(function (response) {
+        if(response.data.status == 200){
+            document.getElementsByClassName('player1Current')[0].classList.add('player1');
+            document.getElementsByClassName('player1Current')[0].classList.remove('player1Current');
+            for(let enclos_case of response.data.captured_position){
+                document.getElementById(`c${enclos_case.x+1}r${enclos_case.y+1}`).classList.add('player1');
+            }
+            document.getElementById(`c${response.data.new_position.x+1}r${response.data.new_position.y+1}`).classList.add('player1Current');
+        }else{
+            alert("Move is wrong")
+        }
+    }).catch(function (error) {
+        console.log(error);
+    }).then(function () {
+        // always executed
+    });
 }
